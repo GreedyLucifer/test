d7bcc1ebd88b5b9fe6bcc8c785f4967c83fb9fc7
GreedyLucifer <46939658+GreedyLucifer@users.noreply.github.com>
2019-12-11 16:56:45 -0800

    Add files via upload

---
+++ computeQ.cu
@@ -0,0 +1,115 @@
+/***************************************************************************
+ *cr
+ *cr            (C) Copyright 2007 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ***************************************************************************/
+
+#define PI   3.1415926535897932384626433832795029f
+#define PIx2 6.2831853071795864769252867665590058f
+
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define K_ELEMS_PER_GRID 2048
+
+#define PHIMAG_BLOCKDIM 512
+#define COMPUTEQ_BLOCKDIM 256
+#define COMPUTEQ_K_ELEMS_PER_GRID 1024
+
+struct kValues {
+  float Kx;
+  float Ky;
+  float Kz;
+  float PhiMag;
+};
+/* Values in the k-space coordinate system are stored in constant memory
+ * on the GPU */
+__constant__ __device__ kValues KT[COMPUTEQ_K_ELEMS_PER_GRID];
+
+__global__ void ComputePhiMagGPU(float* phiR, float* phiI, float* phiMag, int numK)
+{
+	int index = blockIdx.x*PHIMAG_BLOCKDIM + threadIdx.x;
+	if (index < numK) 
+	{
+		float real = phiR[index];
+		float imag = phiI[index];
+		phiMag[index] = real * real + imag * imag;
+	}
+}
+
+__global__ void ComputeQKernel(int numK, int KBaseIndex, float* x, float* y, float* z, float* Qr, float* Qi)
+{
+	__shared__ float sx, sy, sz, sQr, sQi;
+	float expArg;
+
+	int Xindex = blockIdx.x * PHIMAG_BLOCKDIM + threadIdx.x;
+
+	sx = x[Xindex];
+	sy = y[Xindex];
+	sz = z[Xindex];
+	sQr = Qr[indexX];
+	sQi = Qi[indexX];
+
+	for (int Kindex=0; Kindex < K_ELEMS_PER_GRID && KBaseIndex < numK; Kindex++)
+	{
+	  expArg = PIx2 * (KT[Kindex].Kx * sx +
+					  KT[Kindex].Ky * sy +
+					  KT[Kindex].Kz * sz);
+	  sQr += KT[Kindex].PhiMag * cosf(expArg);
+	  sQi += KT[Kindex].PhiMag * sinf(expArg);
+	  KBaseIndex++;
+	}
+	Qr[indexX] = sQr;
+	Qi[indexX] = sQi;
+}
+
+void 
+ComputePhiMagCPU(int numK, 
+				 float* phiR_d, float* phiI_d, 
+				 float* phiMag_d) {
+	int phiMagBlocks = numK / PHIMAG_BLOCKDIM;
+	if (numK % KERNEL_PHI_MAG_THREADS_PER_BLOCK)
+		phiMagBlocks++;
+	dim3 DimBlock(PHIMAG_BLOCKDIM, 1, 1);
+	dim3 DimGrid(phiMagBlocks, 1, 1);
+
+	ComputePhiMag_GPU <<< DimGrid, DimBlock >>> (phiR_d, phiI_d, phiMag_d, numK);
+}
+
+void
+ComputeQCPU(int numK, int numX,
+			float* x_d, float* y_d, float* z_d,
+			kValues* kVals,
+			float* Qr_d, float* Qi_d) {
+	int Gridnum = numK / COMPUTEQ_K_ELEMS_PER_GRID;				//tile
+	int Griddim = numX / COMPUTEQ_BLOCKDIM;
+
+	if (numK % COMPUTEQ_K_ELEMS_PER_GRID)
+		Gridnum++;
+	if (numX % COMPUTEQ_BLOCKDIM)
+		Griddim++;
+
+	dim3 DimBlock(COMPUTEQ_BLOCKDIM, 1, 1);
+	dim3 DimGrid(Griddim, 1, 1);
+
+	for (int Grid = 0; Grid < Grids; Grid++) {
+		// Put the tile of K values into constant mem
+		int Base = Grid * COMPUTEQ_K_ELEMS_PER_GRID;
+		kValues* kValsTile = kVals + GridBase;
+		int num = MIN(COMPUTEQ_K_ELEMS_PER_GRID, numK - GridBase);
+
+		cudaMemcpyToSymbol(KT, kValsTile, num * sizeof(kValues), 0);
+
+		ComputeQGPU <<< DimGrid, DimBlock >>> (numK, GridBase, x_d, y_d, z_d, Qr_d, Qi_d);
+	}
+}
+
+void createDataStructsCPU(int numK, int numX, float** phiMag,
+	 float** Qr, float** Qi)
+{
+  *phiMag = (float* ) memalign(16, numK * sizeof(float));
+  *Qr = (float*) memalign(16, numX * sizeof (float));
+  memset((void *)*Qr, 0, numX * sizeof(float));
+  *Qi = (float*) memalign(16, numX * sizeof (float));
+  memset((void *)*Qi, 0, numX * sizeof(float));
+}
---
+++ main.cu
@@ -0,0 +1,182 @@
+/***************************************************************************
+ *cr
+ *cr            (C) Copyright 2007 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ***************************************************************************/
+
+/* 
+ * C code for creating the Q data structure for fast convolution-based 
+ * Hessian multiplication for arbitrary k-space trajectories.
+ *
+ * Inputs:
+ * kx - VECTOR of kx values, same length as ky and kz
+ * ky - VECTOR of ky values, same length as kx and kz
+ * kz - VECTOR of kz values, same length as kx and ky
+ * x  - VECTOR of x values, same length as y and z
+ * y  - VECTOR of y values, same length as x and z
+ * z  - VECTOR of z values, same length as x and y
+ * phi - VECTOR of the Fourier transform of the spatial basis 
+ *      function, evaluated at [kx, ky, kz].  Same length as kx, ky, and kz.
+ *
+ * recommended g++ options:
+ *  -O3 -lm -ffast-math -funroll-all-loops
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <malloc.h>
+
+#include "parboil.h"
+
+#include "file.h"
+#include "computeQ.cc"
+
+int
+main (int argc, char *argv[]) {
+  int numX, numK;		/* Number of X and K values */
+  int original_numK;		/* Number of K values in input file */
+  float *kx, *ky, *kz;		/* K trajectory (3D vectors) */
+  float *x, *y, *z;		/* X coordinates (3D vectors) */
+  float *phiR, *phiI;		/* Phi values (complex) */
+  float *phiMag;		/* Magnitude of Phi */
+  float *Qr, *Qi;		/* Q signal (complex) */
+  struct kValues* kVals;
+
+  struct pb_Parameters *params;
+  struct pb_TimerSet timers;
+
+  pb_InitializeTimerSet(&timers);
+
+  /* Read command line */
+  params = pb_ReadParameters(&argc, argv);
+  if ((params->inpFiles[0] == NULL) || (params->inpFiles[1] != NULL))
+    {
+
+      fprintf(stderr, "Expecting one input filename\n");
+      exit(-1);
+    }
+  
+  /* Read in data */
+  pb_SwitchToTimer(&timers, pb_TimerID_IO);
+  inputData(params->inpFiles[0],
+	    &original_numK, &numX,
+	    &kx, &ky, &kz,
+	    &x, &y, &z,
+	    &phiR, &phiI);
+
+  /* Reduce the number of k-space samples if a number is given
+   * on the command line */
+  if (argc < 2)
+    numK = original_numK;
+  else
+    {
+      int inputK;
+      char *end;
+      inputK = strtol(argv[1], &end, 10);
+      if (end == argv[1])
+	{
+	  fprintf(stderr, "Expecting an integer parameter\n");
+	  exit(-1);
+	}
+
+      numK = MIN(inputK, original_numK);
+    }
+
+  printf("%d pixels in output; %d samples in trajectory; using %d samples\n",
+         numX, original_numK, numK);
+
+  pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
+
+  /* Create CPU data structures */
+  createDataStructsCPU(numK, numX, &phiMag, &Qr, &Qi);
+
+	float *phiR_d, *phiI_d;
+    float *phiMag_d;
+
+    cudaMalloc ((void **) &phiR_d, numK * sizeof(float));
+    cudaMemcpy (phiR_d, phiR, numK * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaMalloc ((void **) &phiI_d, numK * sizeof(float));
+    cudaMemcpy (phiI_d, phiI, numK * sizeof(float), cudaMemcpyHostToDevice);
+    
+    cudaMalloc((void **)&phiMag_d, numK * sizeof(float));
+
+    cudaThreadSynchronize();
+
+    computePhiMag_GPU(numK, phiR_d, phiI_d, phiMag_d);
+
+    cudaThreadSynchronize();
+
+    cudaMemcpy (phiMag, phiMag_d, numK * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(phiMag_d);
+    cudaFree(phiR_d);
+    cudaFree(phiI_d);
+
+  kVals = (struct kValues*)calloc(numK, sizeof (struct kValues));
+  int k;
+  for (k = 0; k < numK; k++) {
+    kVals[k].Kx = kx[k];
+    kVals[k].Ky = ky[k];
+    kVals[k].Kz = kz[k];
+    kVals[k].PhiMag = phiMag[k];
+  }
+
+	float *x_d, *y_d, *z_d;
+    float *Qr_d, *Qi_d;
+	kValues* kVals_d;
+
+    cudaMalloc ((void **) &x_d, numX * sizeof(float));
+    cudaMemcpy (x_d, x, numX * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaMalloc ((void **) &y_d, numX * sizeof(float));
+    cudaMemcpy (y_d, y, numX * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaMalloc ((void **) &z_d, numX * sizeof(float));
+    cudaMemcpy (z_d, z, numX * sizeof(float), cudaMemcpyHostToDevice);
+
+
+    cudaMalloc((void **)&kVals_d, numK * sizeof(struct kValues));
+    cudaMemcpy(kVals_d, kVals, numK * sizeof(struct kValues), cudaMemcpyHostToDevice);
+
+    cudaMalloc((void **)&Qr_d, numX * sizeof(float));
+    cudaMemset((void *)Qr_d, 0, numX * sizeof(float));
+    cudaMalloc((void **)&Qi_d, numX * sizeof(float));
+    cudaMemset((void *)Qi_d, 0, numX * sizeof(float));
+    cudaDeviceSynchronize();
+
+	computeQGPU(numK, numX, kVals_d, x_d, y_d, z_d, Qr_d, Qi_d);
+	cudaDeviceSynchronize();
+
+  if (params->outFile)
+    {
+      /* Write Q to file */
+      pb_SwitchToTimer(&timers, pb_TimerID_IO);
+      outputData(params->outFile, Qr, Qi, numX);
+      pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
+    }
+
+  free (kx);
+  free (ky);
+  free (kz);
+  free (x);
+  free (y);
+  free (z);
+  free (phiR);
+  free (phiI);
+  free (phiMag);
+  free (kVals);
+  free (Qr);
+  free (Qi);
+
+  pb_SwitchToTimer(&timers, pb_TimerID_NONE);
+  pb_PrintTimerSet(&timers);
+  pb_FreeParameters(params);
+
+  return 0;
+}
